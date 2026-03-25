de9de439fc36a9d66c366121b3078503df02cf67
shedaniel <daniel@shedaniel.me>
2026-03-26 02:29:08 +0900

    Fix margins on profile page

--- src/components/profile-page.tsx
+++ src/components/profile-page.tsx
@@ -77,7 +77,7 @@ export function ProfilePage({
   };
 
   return (
-    <div className="container mx-auto max-w-[1300px] px-4 py-8">
+    <div className="container mx-auto max-w-[1300px] px-3 md:px-6 lg:px-12 py-8">
       <Header
         currentTab="dashboard"
       />
